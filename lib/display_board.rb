b6651274cf450a75333b7085dba215270f7153d6
Jonathan East <jonathan_east@ymail.com>
2020-03-28 17:22:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # out the current state.
 def display_board(board)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+  rows = ["#{board[0]}|#{board[1]}|#{board[2]}", "-----------", "#{board[3]}|#{board[4]}|#{board[5]}", "-----------", "#{board[6]}|#{board[7]}|#{board[8]}"]
 
   puts ("   |   |   ")
   puts ("-----------")
