b63a6b282baf6e0bde3f013be5db5e4f506cfc9c
Jonathan East <jonathan_east@ymail.com>
2020-03-28 17:02:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+  puts ("-----------")
+  puts ("   |   |   ")
+
   board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
-
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
-
 end
