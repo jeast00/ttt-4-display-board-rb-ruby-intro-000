917efab952a68c6363bca3778aa450aa0c48af2c
Jonathan East <jonathan_east@ymail.com>
2020-03-28 17:02:04 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,12 +9,4 @@ def display_board(board)
   puts ("-----------")
   puts ("   |   |   ")
 
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
-  puts ("-----------")
-  puts ("   |   |   ")
-
-
-
 end
