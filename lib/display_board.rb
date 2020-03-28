2673dcbd34cbc610a67f43c7bd1aab561366ef45
Jonathan East <jonathan_east@ymail.com>
2020-03-28 17:48:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,11 +12,11 @@ def display_board(board)
 end
 
 def display_board(board)
-  board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
   puts ("   |   |   ")
   puts ("-----------")
-  puts ("   |   |   ")
+  puts ("   | X |   ")
   puts ("-----------")
   puts ("   |   |   ")
 end
